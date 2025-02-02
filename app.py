471cd452a887d376ad4651907f3b58e0d8e401ab
netanelyasi <126358650+netanelyasi@users.noreply.github.com>
2025-02-02 11:23:36 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({'message': 'Hello, World!'}), 200
+
+if __name__ == '__main__':
+    # הפעלת השרת על כל הממשקים, ב־port 5000, עם אפשרות debug
+    app.run(host='0.0.0.0', port=5000, debug=True)
